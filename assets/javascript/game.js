52c387b438890707807c569534a8c59d6ee22aaa
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-04-26 23:11:38 -0700

    4262

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,45 @@
 var words =["Triceratops", "Tyrannosaurus", "Brontosaurus","Pterodactyl","Mastadon","Sabertooth", "Gastonia", "Rajasaurus", "Stegosaurus", "Confuciusornis", "Fukuisaurus", "Velociraptor", "Iguanodon", "Nodosaurus", "Riojasaurus", "Tarchia", "Tuojiangosaurus", "Eotyrannus", "Brachiosaurus", "Mammoth"]
-var bank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var match= ["T","r","i","c","e","r","a","t","o","p","s"];
-var dashes = "";
-var sBtn = document.getElementById("Start");
-var nBtn = document.getElementById("New");
-var current = document.getElementById("WORD").innerHTML;
-var newWord=words[Math.floor(Math.random()*words.length)];
-var possibleWord=newWord;
-var guessesLeft= document.getElementById("remain");
-var lettersGuessed= document.getElementById("guessed");
-var wins= document.getElementById("wins");
-var wrong=[];
-
-function pickAWord(){
-	var newWord = words[Math.floor(Math.random()*20)];
-	return newWord;
-
-}
-
-
-document.onclick=function Game(){
-document.getElementById("Start").onclick = pickAWord();
-document.getElementById("WORD").innerHTML=pickAWord();
-document.getElementById("remain").innerHTML= 0;
-document.getElementById("guessed").innerHTML= wrong;
-document.getElementById("wins").innerHTML= 0;
-}
-
+var newWord=words[Math.floor(Math.random()*20)];
+var answerArray=[];
+var wrongLetter=[];
+var winCounter= 0 ;
+var guessRemaining= 8;
+var wrongLetters= document.getElementById("wrongArray");
+var winCounter= document.getElementById("winCounter");
+var guessRemaining= document.getElementById("guessCount");
+var dashes="";
+var answer= document.getElementById("WORD");
 
 document.onkeyup = function (event) {
+
   var bank = String.fromCharCode(event.keyCode).toLowerCase();
   console.log(bank);
 }
 
+function pickAWord(){
+	var newWord = words[Math.floor(Math.random()*20)];
+	document.getElementById("WORD").innerHTML= newWord;
+	wrongLetters.innerHTML= " None" ;
+	winCounter.innerHTML=  0;
+	guessRemaining.innerHTML= 8;
+	for (var i = 0; i < newWord.length; i++) {
 
-
-function underscore(newWord){
-	var dashes="";
-for (i = 0; i < newWord.length; i++) {
-  if (match.indexOf(newWord[i])!== -1) {
-dashes += newWord[i];
-  }
-  else {
-  	dashes += "_";
-  }
-}
+		answerArray=newWord.length;
+      	answerArray.push("_");
+  };
+      answer.innerHTML=answerArray;
+	
+	console.log(newWord);
+    
 };
+
+
+
+document.getElementById("Start").onclick = function(){
+	pickAWord(); 
+};
+
+
+
+
+	
--- assets/javascript/untitled.js
+++ assets/javascript/untitled.js
@@ -1,51 +1,85 @@
+var words =["Triceratops", "Tyrannosaurus", "Brontosaurus","Pterodactyl","Mastadon","Sabertooth", "Gastonia", "Rajasaurus", "Stegosaurus", "Confuciusornis", "Fukuisaurus", "Velociraptor", "Iguanodon", "Nodosaurus", "Riojasaurus", "Tarchia", "Tuojiangosaurus", "Eotyrannus", "Brachiosaurus", "Mammoth"]
+var newWord=words[Math.floor(Math.random()*20)];
+var answerArray=[];
+var wrongAnswer=[];
+var remainingLetters= newWord.length;
+var wrongLetter=[];
+var winCounter=[];
+var guessRemaining=[];
+var wrongLetter= document.getElementbyId("wrongLetters");
+var winCounter= document.getElementbyId("winCounter");
+var guessRemaining= document.getElementbyId("guessCount");
+
+////
+var words =["Triceratops", "Tyrannosaurus", "Brontosaurus","Pterodactyl","Mastadon","Sabertooth", "Gastonia", "Rajasaurus", "Stegosaurus", "Confuciusornis", "Fukuisaurus", "Velociraptor", "Iguanodon", "Nodosaurus", "Riojasaurus", "Tarchia", "Tuojiangosaurus", "Eotyrannus", "Brachiosaurus", "Mammoth"]
+var bank = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var match= ["T","r","i","c","e","r","a","t","o","p","s"];
+var dashes = [];
 var sBtn = document.getElementById("Start");
 var nBtn = document.getElementById("New");
+var current = document.getElementById("WORD").innerHTML;
+var newWord=words[Math.floor(Math.random()*words.length)];
+var guessesLeft= document.getElementById("remain");
+var lettersGuessed= document.getElementById("guessed");
+var wins= document.getElementById("wins");
+var wrong=[];
+var newWord=words[Math.floor(Math.random()*20)];
 
-function Game(){
-document.getElementById("Start").onclick = pickAWord();
-document.getElementById("WORD").innerHTML=pickAWord();
-
-}
-
-function Game(){
-sBtn.addEventListener("click" , pickAWord);
-current.innerHTML=pickAWord();
-}
-
-
-var sBtn = document.getElementById("Start");
-var nBtn = document.getElementById("New");
-var current = document.getElementById("WORD").innerHTML=pickAWord();
-
-function Game(){
-document.getElementById("Start").onclick = pickAWord();
-document.getElementById("WORD").innerHTML=pickAWord();
-
-}
 
 function pickAWord(){
 	var newWord = words[Math.floor(Math.random()*20)];
 	return newWord;
 
-}
-
-
-function underscore(newWord match){
-	var dashes="";
-for (i = 0; i < newWord.length; i++) {
-  if (match.indexOf(newWord[i])!== -1) {
-dashes += newWord[i];
-  }
-  else {
-  	dashes += "_";
-  }
-}
 };
 
 
-document.onkeyup = function (event) {
-  var lGuess.innerHTML = String.fromCharCode(event.keyCode).toLowerCase();
-};
+
+document.onclick=function Game(){
+document.getElementById("Start").onclick = pickAWord();
+document.getElementById("WORD").innerHTML=pickAWord();
+document.getElementById("remain").innerHTML= 0;
+document.getElementById("guessed").innerHTML= wrong;
+document.getElementById("wins").innerHTML= 0;
+}
 
 
-_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"
+var input= document.onkeyup = function (event) {
+  var bank = String.fromCharCode(event.keyCode).toLowerCase();
+  console.log(bank);
+}
+
+
+
+
+
+/////
+
+
+
+for (var i = 0; i < newWord.length; i++) {
+
+      answerArray[i] = "_";
+
+    }
+while(remainingLetters > 0){
+remainingLetters--;
+	answerArray.join(" ").innerHTML;
+
+}
+
+for (var j = 0; j < newWord.length; j++) {
+
+          if (newWord[j] === guess) {
+
+            answerArray[j] = guess;
+
+            remainingLetters--;
+
+          }
+      };
+
+      //     else if (newWord[j] !== guess) {
+      //     	guess.push(wrongAnswer);
+          	
+      //     }
+      // };
