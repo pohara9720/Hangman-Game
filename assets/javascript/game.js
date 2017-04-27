fb460115f8855d66ea9505ce46a327b3903f88ef
Patrick O'Hara <Patrick@Patricks-MacBook-Pro.local>
2017-04-27 10:56:49 -0700

    427

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,6 +10,7 @@ var guessRemaining= document.getElementById("guessCount");
 var dashes="";
 var answer= document.getElementById("WORD");
 
+
 document.onkeyup = function (event) {
 
   var bank = String.fromCharCode(event.keyCode).toLowerCase();
@@ -18,14 +19,14 @@ document.onkeyup = function (event) {
 
 function pickAWord(){
 	var newWord = words[Math.floor(Math.random()*20)];
-	document.getElementById("WORD").innerHTML= newWord;
+	answer.innerHTML= newWord;
 	wrongLetters.innerHTML= " None" ;
 	winCounter.innerHTML=  0;
 	guessRemaining.innerHTML= 8;
 	for (var i = 0; i < newWord.length; i++) {
-
-		answerArray=newWord.length;
-      	answerArray.push("_");
+		answerArray[i]=newWord.length;
+		answerArray[i] = (" _ ");
+      	
   };
       answer.innerHTML=answerArray;
 	
